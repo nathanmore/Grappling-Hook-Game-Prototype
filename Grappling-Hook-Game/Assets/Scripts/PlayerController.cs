559b3c875f0db514cc0c404e1fb2ec7883a10bfc
Nathan More <morenate2000@gmail.com>
2022-05-18 15:20:51 -0700

    Implemented basic grappling hook

--- Grappling-Hook-Game/Assets/Scripts/PlayerController.cs
+++ Grappling-Hook-Game/Assets/Scripts/PlayerController.cs
@@ -10,26 +10,51 @@ public class PlayerController : MonoBehaviour
     [SerializeField] float xClamp = 85f;
     [SerializeField] float sensitivityX = 15f;
     [SerializeField] float sensitivityY = 0.2f;
+    [SerializeField] private Transform debugHitPointTransform;
+    [SerializeField] private float hookshotSpeed = 5f;
+    [SerializeField] private float reachedHookshotPositionDistance;
+    [SerializeField] private float gravityScale;
 
-    CharacterController characterController;
-    float mouseX, mouseY;
-    float xRotation = 0f;
+    private CharacterController characterController;
+    private float mouseX, mouseY;
+    private float xRotation = 0f;
+    private State state;
+    private Vector3 hookshotPosition;
+
+    private enum State { Normal,  HookshotFlyingPlayer}
 
     // Start is called before the first frame update
     void Start()
     {
         characterController = gameObject.GetComponent<CharacterController>();
+        Cursor.lockState = CursorLockMode.Locked;
+        state = State.Normal;
     }
 
     // Update is called once per frame
     void Update()
     {
-        CameraMovement();
+        switch (state)
+        {
+            default: 
+            case State.Normal:
+                CameraMovement();
+                ApplyGravity();
+                break;
+            case State.HookshotFlyingPlayer:
+                HandleHookshotMovement();
+                CameraMovement();
+                break;
+        }
     }
 
     public void OnHook(InputAction.CallbackContext context)
     {
-        Debug.Log("Hook");
+        if (context.performed)
+        {
+            Debug.Log("Hook");
+            HandleHookshotStart();
+        }
     }
     public void OnMouseX(InputAction.CallbackContext context)
     {
@@ -49,6 +74,35 @@ public class PlayerController : MonoBehaviour
         Vector3 targetRotation = transform.eulerAngles;
         targetRotation.x = xRotation;
         playerCamera.eulerAngles = targetRotation;
+    }
+
+    private void ApplyGravity()
+    {
+        characterController.Move(Vector3.up * -gravityScale);
+    }
+
+    private void HandleHookshotStart()
+    {
+        if (Physics.Raycast(playerCamera.transform.position, playerCamera.transform.forward, out RaycastHit raycastHit))
+        {
+            // Hit something
+            debugHitPointTransform.position = raycastHit.point;
+            state = State.HookshotFlyingPlayer;
+            hookshotPosition = raycastHit.point;
+        }
+    }
+
+    private void HandleHookshotMovement()
+    {
+        Vector3 hookshotDir = (hookshotPosition - transform.position).normalized;
+
+        characterController.Move(hookshotDir * hookshotSpeed * Time.deltaTime);
+
+        if (Vector3.Distance(transform.position, hookshotPosition) < reachedHookshotPositionDistance)
+        {
+            // Reached Hookshot Position
+            state = State.Normal;
+        }
     }
 
 
