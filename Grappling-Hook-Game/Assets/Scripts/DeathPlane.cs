019c523318863fdabcdd4e3f491a8969885e9634
Nathan More <morenate2000@gmail.com>
2022-05-31 09:35:37 -0700

    Updated death plane to follow player

--- Grappling-Hook-Game/Assets/Scripts/DeathPlane.cs
+++ Grappling-Hook-Game/Assets/Scripts/DeathPlane.cs
@@ -4,6 +4,20 @@ using UnityEngine;
 
 public class DeathPlane : MonoBehaviour
 {
+    [SerializeField] private Transform playerTransform;
+    private float playerPosBuffer = 100f;
+
+    private void Update()
+    {
+        if (playerTransform != null)
+        {
+            if ((playerTransform.position.z - gameObject.transform.position.z) > playerPosBuffer)
+            {
+                gameObject.transform.position = new Vector3(0, -10, playerTransform.position.z);
+            }
+        }
+    }
+
     public void OnTriggerEnter(Collider other)
     {
         if (other.GetComponent<PlayerController>() != null)
